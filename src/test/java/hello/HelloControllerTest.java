a06c7779bdb5a85677b3b5bbf65a6b50cd161579
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-07-02 13:36:21 +0100

    Update HelloControllerTest.java

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -1,30 +1,22 @@
 package hello;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringRunner;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class HttpRequestTest {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-    @LocalServerPort
-    private int port;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private HomeController controller;
 
     @Test
-    public void greetingShouldReturnDefaultMessage() throws Exception {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("Hello World");
+    public void contexLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
 }
