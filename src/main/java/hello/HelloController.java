b6b19d452baf043ba0201d8cdb6044b307f44a63
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-07-02 10:53:06 +0100

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -13,9 +13,9 @@ public class HelloController {
 
     	@RequestMapping("/")
 	@ResponseBody
-	//String home() {
+	public String home() {
 		return "Hello World!";
-	//}
+	}
 	
 	//@RequestMapping("/more")
 	//@ResponseBody
