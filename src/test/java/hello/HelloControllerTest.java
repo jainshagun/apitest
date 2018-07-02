9700994f6b34b645eee00d817b8ae9c08f1f9aa0
jainshagun <40594574+jainshagun@users.noreply.github.com>
2018-07-02 13:42:21 +0100

    Update HelloControllerTest.java

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmokeTest {
+public class HelloControllerTest {
 
     @Autowired
     private HelloController controller;
